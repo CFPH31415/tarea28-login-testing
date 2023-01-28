36c7bbaec9f1c13d6b29cb926c74b75c09603b42
Carlos Portillo <121558966+CFPH31415@users.noreply.github.com>
2023-01-27 19:03:40 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent implements OnInit {
   title = 'testing';
   formLogin!: FormGroup
+  formValues = { username: '', password: '' };
 
   constructor(private formBuilder: FormBuilder) { }
 
-   ngOnInit(): void {
+  ngOnInit(): void {
     this.formLogin = new FormGroup({
     username: new FormControl('', [Validators.required, Validators.minLength(5)]),
     password: new FormControl('', [Validators.required, Validators.minLength(5)])
     });
     }
-
-}
+    onSubmit() {
+      this.formValues = this.formLogin.value;
+    }
+  }
